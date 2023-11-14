60696e61efc02f4b2e1ce22d50e4641c47579702
ZinoTrust <zinotrust@gmail.com>
2023-11-14 18:46:46 +0100

    initial commit

---
+++ components/Features.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Features = () => {
+  return (
+    <div className="content">
+      <p className="red text-2xl font-bold">This is the Features Page</p>
+    </div>
+  );
+};
+
+export default Features;
